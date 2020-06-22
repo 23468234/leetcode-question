89aa589073517352c8e7b5811e7622c4bf7d20dc
weipf <243468234@qq.com>
2020-06-22 23:22:11 +0800

    0035_SearchInsertPosition/0035_SearchInsertPosition.cpp

--- algorithms/0034_SearchForaRange/SearchForaRange.cpp
+++ algorithms/0034_SearchForaRange/SearchForaRange.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        int mid = 0;
-        int left = 0;
-        int right = nums.size() - 1;
-        while(left <= right)
-        {
-            mid = (left + right) / 2;
-            if (nums[mid] == target){
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int index  = search(nums, 0 ,nums.size()-1, target);
+        if(index == -1) return {-1, -1};
+        // 开始左查找
+        int left_end = index - 1;
+        while(left_end >= 0 && nums[left_end] == target){
+            left_end = search(nums, 0, left_end, target) - 1;
+        }
+        // 开始右查找
+        int right_start = index + 1;
+        while(right_start < nums.size()  && nums[right_start] == target){
+            right_start = search(nums, right_start, nums.size()-1, target) + 1;
+        }
+
+        // 返回结果
+        return {left_end + 1, right_start - 1};
+    }
+private:
+    int search(vector<int>& nums, int start, int end, int target){
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(nums[mid] == target){
                 return mid;
-            }else if (nums[mid] < target)
-            {
-                left = mid + 1;
+            }else if(nums[mid] > target){
+                end = mid - 1;
             }else{
-                right = mid -1;
+                start = mid + 1;
             }
         }
-        return left;
+        return -1;
     }
 };
