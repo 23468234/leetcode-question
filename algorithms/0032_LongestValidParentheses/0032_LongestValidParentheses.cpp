6274026953f6ef2dfc53c500ee7f4afa2e0f977a
weipf <243468234@qq.com>
2020-07-07 23:13:33 +0800

    0032_LongestValidParentheses.cpp

--- algorithms/0032_LongestValidParentheses/0032_LongestValidParentheses.cpp
+++ algorithms/0032_LongestValidParentheses/0032_LongestValidParentheses.cpp
@@ -1,21 +1,77 @@
+
+#if 0
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int maxans = 0;
-        stack<int> stk;
-        stk.push(-1);
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '(') {
-                stk.push(i);
-            } else {
-                stk.pop();
-                if (stk.empty()) {
-                    stk.push(i);
-                } else {
-                    maxans = max(maxans, i - stk.top());
+        int rst = 0;
+
+        stack<int> stack_tmp;
+        stack_tmp.push(-1);
+        for (int i = 0; i < s.length(); ++i){
+            if (s.at(i) == '('){
+                stack_tmp.push(i);
+            }
+            else{
+                stack_tmp.pop();
+
+                if (stack_tmp.empty()){
+                    stack_tmp.push(i);
+                }else{
+                    rst  = rst > i - stack_tmp.top() ? rst : i - stack_tmp.top();
                 }
             }
         }
-        return maxans;
+        
+        return rst;
     }
 };
+#endif
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        if (s.empty()){
+            return 0;
+        }
+
+        std::vector<int>rst(s.length(), 0);
+        for (int i = 0; i < s.length(); ++i){
+            if (s.at(i) == ')'){
+                // 判断挨着的有多长
+                if (i - 1 < 0){
+                    continue;
+                }
+                int len = rst[i - 1];
+
+                // 获取上一个左括号
+                if (i - len - 1 < 0){
+                    continue;
+                }
+
+                if (s.at(i-len-1) != '('){
+                    continue;
+                }                
+                rst[i] = len + 2;
+
+                // 获取左括号左边的长
+                if (i-len-2 < 0){
+                    continue;
+                }                
+
+                rst[i] += rst[i-len-2];
+            }
+        }
+
+        // 遍历rst
+        int target = 0;
+        for (auto value : rst){
+            if (value > target){
+                target = value;
+            }
+        }
+
+        return target;
+    }
+};
+
+
